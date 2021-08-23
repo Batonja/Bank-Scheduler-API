20b3f0ba2c598ae8266389c513adf9349c47c0fe
Nedeljko <nedeljko.zekonja@marbleit.rs>
2021-08-23 22:16:40 +0200

    Cron job
    Cron that fires every hour

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import config from "../config.json";
 import ErrorController from "./controllers/ErrorController";
 import connectToDB from "./database/connectToDB";
 import mainRouter from "./routers/MainRouter";
+import { CronJob } from "cron";
+import MainService from "./services/implementations/MainService";
 
 const app = express();
 app.use(express.json());
@@ -10,6 +12,10 @@ app.use(mainRouter);
 app.use(ErrorController.handleError);
 
 connectToDB();
+
+const job = new CronJob("0 0 * 1-31 0-11 0-6", () => {
+  MainService.consumeFromQueue();
+});
 
 app.listen(config.port, () => {
   console.log(`Application is listening on port:${config.port}`);
